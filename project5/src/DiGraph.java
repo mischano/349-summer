85bf621e9da143f11d8e04056801bc9fab68d05d
mischano <mansur.ischanov@gmail.com>
2022-07-23 19:35:43 -0700

    done

--- project5/src/DiGraph.java
+++ project5/src/DiGraph.java
@@ -1,5 +1,11 @@
+// Mansur Ischanov (mischano@calpoly.edu) & Luis 
+// July 23rd, 2022
+// Project 5
+
+import java.util.Collections;
 import java.util.LinkedList;
 
+@SuppressWarnings("unchecked")
 public class DiGraph {
     private final LinkedList<Integer>[] ll;
 
@@ -89,7 +95,70 @@ public class DiGraph {
 
         return sll;
     }
-    
+
+    private VertexInfo[] BFS(int s) {
+        VertexInfo[] vi = new VertexInfo[ll.length];
+        LinkedList<Integer> rq = new LinkedList<>();
+
+        for (int i = 0; i < ll.length; i++) {
+            vi[i] = new VertexInfo(-1, -1);
+        }
+
+        vi[s].pathLength = 0;
+        rq.addLast(s);
+
+        while (!rq.isEmpty()) {
+            int i = rq.removeFirst();
+            for (int j : ll[i]) {
+                if (-1 == vi[j].pathLength) {
+                    vi[j].pathLength = vi[i].pathLength + 1;
+                    vi[j].vertexParent = i;
+                    rq.addLast(j);
+                }
+            }
+        }
+        return vi;
+    }
+
+    public boolean isTherePath(int from, int to) {
+        VertexInfo[] vi = BFS(from - 1);
+        return (vi[to - 1].vertexParent != -1 || from == to);
+    }
+
+    public int lengthOfPath(int from, int to) {
+        VertexInfo[] vi = BFS(from - 1);
+
+        int i = to - 1;
+        if ((vi[i].vertexParent == -1) && (from != to)) {
+            return -1;
+        }
+
+        int j = 0;
+        while (-1 != vi[i].vertexParent) {
+            j++;
+            i = vi[i].vertexParent;
+        }
+
+        return j;
+    }
+
+    public void printPath(int from, int to) {
+        VertexInfo[] vi = BFS(from - 1);
+
+        if ((vi[to - 1].vertexParent == -1) && (from != to)) {
+            System.out.println("There is no path");
+            return;
+        }
+
+        StringBuilder result = new StringBuilder();
+        int i = to - 1;
+        while (vi[i].vertexParent != -1) {
+            result.insert(0, " to " + (i + 1));
+            i = vi[i].vertexParent;
+        }
+        System.out.println("" + from + result);
+    }
+
     public void print() {
         for (int i = 0; i < ll.length; i++) {
             System.out.format("" + (i + 1) + " is connected to:");
@@ -105,83 +174,58 @@ public class DiGraph {
             System.out.format("\n");
         }
     }
-    //////////////////////////////////////Part 3
-    private VertexInfo[] BFS(int s)
-    {
-        int n = ll.length;
-        VertexInfo[] VA = new VertexInfo[n];
 
+    private TreeNode buildTree(int s) {
+        VertexInfo[] paths = BFS(s - 1);
+        TreeNode[] tree = new TreeNode[paths.length];
 
-        for(int i = 1; i < n; i++)
-        {
-            VA[i].distance = -1;
-            VA[i].predecessor = -1;
+        for (int i = 0; i < tree.length; i++) {
+            tree[i] = new TreeNode(i + 1);
         }
-        VA[s].distance = 0;
-        LinkedList<Integer> regQueue = new LinkedList<>();
-        regQueue.addLast(s);
-        while(!regQueue.isEmpty())
-        {
-            int u = regQueue.removeFirst();
-            for(int v : ll[u])
-                if(VA[v].distance == -1)
-                {
-                    VA[v].distance = VA[u].distance + 1;
-                    VA[v].predecessor = u;
-                    regQueue.addLast(v);
-                }
-        }
-
-        return VA;
-
-
-    }
-
-   
-
-    //method returns shortest distance from vertex to vertex
-    public int lengthOfPath(int from, int to)
-    {
-        int vDistance = 0;
-        VertexInfo[] VA  = BFS(from);
-          if (VA[to].distance == -1)
-          {
-              vDistance = 0;
-          }
-          else
-              while(from != to)
-              {
-                  to = VA[to].predecessor;
-              }
-              vDistance = from + vDistance;
-
-
-     return vDistance;
-    }
-
-    public void printPath(int from, int to)
-    {
-        String printP = null;
-        VertexInfo[] VA  = BFS(from);
-        if (VA[to].distance == -1)
-        {
-            System.out.println("No path");
-        }
-        else
-            printP = "";
-            while(from != to)
-            {
-                printP ="->" + to + printP;
-                to = VA[to].predecessor;
+        for (int i = 0; i < paths.length; i++) {
+            int parent = paths[i].vertexParent;
+            if (parent != -1) {
+                tree[parent].addChild(tree[i]);
             }
-        printP = from + printP;
-        System.out.println(printP);
+        }
+
+        return tree[s - 1];
     }
 
+    public void printTree(int s) {
+        TreeNode root = buildTree(s);
+        System.out.println(root.vertexNumber);
+        printTreeRecursive(root, 1);
+    }
 
-    public class VertexInfo
-    {
-        private int distance;
-        private int predecessor;
+    private void printTreeRecursive(TreeNode current, int d) {
+        for (TreeNode child : current.vertexChildren) {
+            System.out.println(String.join("", Collections.nCopies(d, "\t")) + child.vertexNumber);
+            printTreeRecursive(child, d + 1);
+        }
+    }
+
+    private static class TreeNode {
+        int vertexNumber;
+        LinkedList<TreeNode> vertexChildren;
+
+        private TreeNode(int n) {
+            this.vertexNumber = n;
+            this.vertexChildren = new LinkedList<>();
+        }
+
+        private void addChild(TreeNode child) {
+            this.vertexChildren.add(child);
+        }
+    }
+
+    private static class VertexInfo {
+        public int pathLength;
+        public int vertexParent;
+
+        public VertexInfo(int i, int j) {
+            this.pathLength = i;
+            this.vertexParent = j;
+        }
     }
 }
--- project5/src/DiGraphTest.java
+++ project5/src/DiGraphTest.java
@@ -1,25 +1,30 @@
+// Mansur Ischanov (mischano@calpoly.edu) & Luis 
+// July 23rd, 2022
+// Project 5
+
 import java.util.Scanner;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class DiGraphTest {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         System.out.println("Enter the number of vertices in the directed graph:");
         Scanner in = new Scanner(System.in);
         String input = in.next();
         DiGraph dg = new DiGraph(Integer.parseInt(input));
+        //noinspection TextBlockMigration
         System.out.println("Choose one of the following operations:\n" +
-        "- add edge (enter a)\n" +
-        "- delete edge (enter d)\n" +
-        "- edge count (enter e)\n" +
-        "- vertex count (enter v)\n" +
-        "- print graph (enter p)\n" +
-        "- topological sort (enter t)\n" +
-        "- is there a path (enter i)\n" +
-        "- length of the path (enter l)\n" +
-        "- shortest path (enter s)\n" +
-        "- print breadth-first-tree (enter b)\n" +
-        "- Quit (enter q)");
+                "- add edge (enter a)\n" +
+                "- delete edge (enter d)\n" +
+                "- edge count (enter e)\n" +
+                "- vertex count (enter v)\n" +
+                "- print graph (enter p)\n" +
+                "- topological sort (enter t)\n" +
+                "- is there a path (enter i)\n" +
+                "- length of the path (enter l)\n" +
+                "- shortest path (enter s)\n" +
+                "- print breadth-first-tree (enter b)\n" +
+                "- Quit (enter q)");
         while (!input.equals("q")) {
             do {
                 input = in.nextLine();
@@ -63,16 +68,52 @@ public class DiGraphTest {
                                 IntStream.of(vertices).mapToObj(Integer::toString).collect(Collectors.joining(", ")));
                     }
                     break;
+                case "i":
+                    System.out.println("Enter start and destination vertex");
+                    from = in.nextInt();
+                    to = in.nextInt();
+                    in.nextLine();
+                    boolean path = dg.isTherePath(from, to);
+                    if (path) {
+                        System.out.println("There is a path from " + from + " to " + to + ".");
+                    } else {
+                        System.out.println("There is not a path from " + from + " to " + to + ".");
+                    }
+                    break;
+                case "l":
+                    System.out.println("Enter start and destination vertex");
+                    from = in.nextInt();
+                    to = in.nextInt();
+                    in.nextLine();
+                    int length = dg.lengthOfPath(from, to);
+                    System.out.println("The path length from " + from + " to " + to + " is " + length + ".");
+                    break;
+                case "s":
+                    System.out.println("Enter start and destination vertex");
+                    from = in.nextInt();
+                    to = in.nextInt();
+                    in.nextLine();
+                    path = dg.isTherePath(from, to);
+                    if (path) {
+                        System.out.println("The shortest path is the following:");
+                        dg.printPath(from, to);
+                    } else {
+                        System.out.println("There is not a path from " + from + " to " + to + ".");
+                    }
+                    break;
+                case "b":
+                    System.out.println("Enter the source vertex number:");
+                    int vertex = in.nextInt();
+                    System.out.println("The breadth-first-tree is the following:");
+                    dg.printTree(vertex);
+                    break;
                 case "q":
                     continue;
                 default:
-                System.out.println("InputMismatchException: Invalid menu choice!");
+                    System.out.println("InputMismatchException: Invalid menu choice!");
                     break;
-                }
             }
-        System.out.println("Good bye.");
         }
+        System.out.println("Good bye.");
     }
-
-
-
+}
